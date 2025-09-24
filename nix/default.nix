afa63cb8e62a4821e3e45d0e3f8afa5c0b524713
Luis González <5774777+luis-agm@users.noreply.github.com>
2025-09-24 09:48:41 +0200

    Overlay pkgs and simplify modules.

--- nix/default.nix
+++ nix/default.nix
@@ -1,9 +1,4 @@
 {
-    self,
-    dgop,
-    dms-cli,
-    ...
-}: {
     config,
     pkgs,
     lib,
@@ -67,7 +62,7 @@ in {
             package = cfg.quickshell.package;
 
             configs.dms = "${
-                self.packages.${pkgs.system}.dankMaterialShell
+                pkgs.dankMaterialShell
             }/etc/xdg/quickshell/DankMaterialShell";
             activeConfig = lib.mkIf cfg.enableSystemd "dms";
 
@@ -86,9 +81,10 @@ in {
                 pkgs.ddcutil
                 pkgs.libsForQt5.qt5ct
                 pkgs.kdePackages.qt6ct
-                dms-cli.packages.${pkgs.system}.default
+
+                pkgs.dmsCli
             ]
-            ++ lib.optional cfg.enableSystemMonitoring dgop.packages.${pkgs.system}.dgop
+            ++ lib.optional cfg.enableSystemMonitoring pkgs.dgop
             ++ lib.optionals cfg.enableClipboard [pkgs.cliphist pkgs.wl-clipboard]
             ++ lib.optionals cfg.enableVPN [pkgs.glib pkgs.networkmanager]
             ++ lib.optional cfg.enableBrightnessControl pkgs.brightnessctl
