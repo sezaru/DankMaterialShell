50e64a84e918220c9954f94558039c9f1d2d0397
Eduardo Barreto Alexandre <blibs@blobs.com>
2025-09-25 20:26:41 -0300

    fewji

--- nix/configs/default.nix
+++ nix/configs/default.nix
@@ -1,13 +1,11 @@
-{ self, cfg, lib, pkgs, dgop, dms-cli, ... }:
+{ cfg, lib, pkgs, ... }:
 
 {
   programs.quickshell = {
     enable = true;
     package = cfg.quickshell.package;
 
-    configs.dms = "${
-      self.packages.${pkgs.system}.dankMaterialShell
-    }/etc/xdg/quickshell/DankMaterialShell";
+    configs.dms = "${pkgs.dankMaterialShell}/etc/xdg/quickshell/DankMaterialShell";
     activeConfig = lib.mkIf cfg.enableSystemd "dms";
 
     systemd = lib.mkIf cfg.enableSystemd {
@@ -25,9 +23,9 @@
       pkgs.ddcutil
       pkgs.libsForQt5.qt5ct
       pkgs.kdePackages.qt6ct
-      dms-cli.packages.${pkgs.system}.default
+      pkgs.dmsCli
     ]
-    ++ lib.optional cfg.enableSystemMonitoring dgop.packages.${pkgs.system}.dgop
+    ++ lib.optional cfg.enableSystemMonitoring pkgs.dgop
     ++ lib.optionals cfg.enableClipboard [pkgs.cliphist pkgs.wl-clipboard]
     ++ lib.optionals cfg.enableVPN [pkgs.glib pkgs.networkmanager]
     ++ lib.optional cfg.enableBrightnessControl pkgs.brightnessctl
--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,7 @@
-{ self, dgop, dms-cli, ... }: {config, pkgs, lib, ...}: let
+{config, pkgs, lib, ...}: let
   cfg = config.programs.dankMaterialShell;
 in {
   options.programs.dankMaterialShell = (import ./options.nix { inherit lib pkgs; }).basic;
 
-  config = lib.mkIf cfg.enable (import ./configs/default.nix { inherit self cfg lib pkgs dgop dms-cli; });
+  config = lib.mkIf cfg.enable (import ./configs/default.nix { inherit cfg lib pkgs; });
 }
